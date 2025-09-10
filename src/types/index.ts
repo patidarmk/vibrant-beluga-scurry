3f003209a9e0c8f826d77ac137573bcde61e2790
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-10 21:19:15 +0530

    Update src/types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,12 @@ export type Todo = {
   text: string;
   completed: boolean;
 };
+
+export type WeatherData = {
+  temperature: number;
+  windspeed: number;
+  winddirection: number;
+  weathercode: number;
+  is_day: number;
+  time: string;
+};
